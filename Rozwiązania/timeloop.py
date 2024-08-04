759a5950f6e17a3d5ca7504c847d37f5fd4b658f
WyginaczBlachy <2001selby@gmail.com>
2024-08-04 12:02:57 +0200

    Delete Rozwiązania directory

--- Rozwiązania/timeloop.py
+++
@@ -1,5 +0,0 @@
-number = int(input(''))
-i = 1
-while i <= number:
-    print(str(i) + (' Abracadabra'))
-    i = i + 1
--- Rozwiązania/twostones.py
+++
@@ -1,7 +0,0 @@
-
-Num_of_stones = int(input())
-if (Num_of_stones % 2) == 0:
-    print("Bob"),
-else:
-    print("Alice")
-    
