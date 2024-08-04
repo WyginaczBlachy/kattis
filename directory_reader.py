1d70d2f82c663dab1a2b54d73ce2d9685589e92e
WyginaczBlachy <2001selby@gmail.com>
2024-08-04 15:33:20 +0200

    Updated README.md

--- directory_reader.py
+++ directory_reader.py
@@ -5,16 +5,15 @@ class ListingDirectory:
         self.path_file = path_file
 
     def file_extension_checker(self, file_target):
-        accepted_extensions = ['py']  # change here for another extension
+        accepted_extensions = ['py']
         if '.' not in file_target:
             return False
 
         file_target_split = file_target.split('.')
-        target = file_target_split[-1] # get the last part after the last dot
+        target = file_target_split[-1]
         return target in accepted_extensions
 
     def make_dictionary(self):
-        # Check if the given path exists
         if not os.path.exists(self.path_file):
             print(f"Path {self.path_file} does not exist.")
             return {}
@@ -26,8 +25,8 @@ class ListingDirectory:
 
         source_file = {}
         for folder in source_code:
-            folder_path = os.path.join(self.path_file, folder)  # Properly join the path
-            if os.path.isdir(folder_path):  # Only process if it is a directory
+            folder_path = os.path.join(self.path_file, folder)
+            if os.path.isdir(folder_path):
                 tmp = []
                 try:
                     solution_code = os.listdir(folder_path)
@@ -36,8 +35,8 @@ class ListingDirectory:
                     continue
 
                 for code in solution_code:
-                    if self.file_extension_checker(code):  # No need to compare with True
-                        tmp.append(code)  # Corrected line here
+                    if self.file_extension_checker(code):
+                        tmp.append(code)
                 source_file[folder] = tmp
 
         return source_file
