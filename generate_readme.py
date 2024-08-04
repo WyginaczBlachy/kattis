f54a36aa1ec4f7993ffec8fbad4d4830267a8962
WyginaczBlachy <2001selby@gmail.com>
2024-08-04 13:06:58 +0200

    Update generate_readme.py

--- generate_readme.py
+++ generate_readme.py
@@ -1,13 +1,14 @@
-
 import directory_reader
 import kattis_scrapper
 from datetime import date
 
-inputSource = directory_reader.ListingDirectory('Kattis')
+# Initialize the directory reader with the path to the Kattis directory
+inputSource = directory_reader.ListingDirectory(r'C:\Users\2001s\PycharmProjects\Giraffe\Kattis')
 dictSource = inputSource.make_dictionary()
 
-def linkProblems(problem_name, solution):
-    problem_link = 'https:open.kattis.com/problems/{}'.format(problem_name)
+# Define functions for problem and solution links
+def linkProblems(problem_name):
+    problem_link = 'https://open.kattis.com/problems/{}'.format(problem_name)
     return problem_link
 
 def linkSolutions(problem_name, solution):
@@ -17,50 +18,55 @@ def linkSolutions(problem_name, solution):
     extension = None
     if '.py' in solution:
         extension = 'Python'
-    readme.link = '[{}]({})'.format(extension, link)
+    readme_link = '[{}]({})'.format(extension, link)
     return readme_link
+
 def writingReadme(option):
     if option.lower() == 'y':
         scrapper = kattis_scrapper.PointScrapper()
-        with open('README.md', 'w', encoding='UTF-8') as f:
-            tmp = 0
-            f.write('# Kattis rozwiązania \n')
-            f.write('Rozwiazania problemów z [Kattis](https://open.kattis.com/). \n\n')
-            f.write('Zaktualizowano: ' + str(date.today().strftime("%B %d, %Y")) + '\n\n')
-            if option.lower() == 'y':
-                f.write(' | LP | Problem | Rozwiązanie | Trudność |\n')
-                f.write(' | __ | ------- | ----------- | -------- |\n')
-            else:
-                f.write(' | LP | Problem | Rozwiązanie |\n')
-                f.write(' | -- | ------- | ----------- |\n')
-            for key in dictSource:
-                list_tmp = dictSource[key]
-                tmp += 1
-                f.write(' | ' + str(tmp))
-                target = list.tmp[0].split('.')
-                target = target[0]
-                problem_link = linkProblems(target)
-                f.write(' | [{}]({}) | '.format(key, problem_link))
-                for i in range(len(list_tmp)):
-                    if i == len(list_tmp) - 1:
-                        f.write(linkSolutions(key, list_tmp[i]) + ' ')
-                    else:
-                        f.write(linkSolutions(key, list_tmp[i]) + ', ')
-                f.write('|')
+        try:
+            with open('README.md', 'w', encoding='UTF-8') as f:
+                tmp = 0
+                f.write('# Kattis rozwiązania \n')
+                f.write('Rozwiazania problemów z [Kattis](https://open.kattis.com/). \n\n')
+                f.write('Zaktualizowano: ' + str(date.today().strftime("%B %d, %Y")) + '\n\n')
                 if option.lower() == 'y':
-                    f.write(scrapper.getPoints(problem_link))
+                    f.write(' | LP | Problem | Rozwiązanie | Trudność |\n')
+                    f.write(' | __ | ------- | ----------- | -------- |\n')
+                else:
+                    f.write(' | LP | Problem | Rozwiązanie |\n')
+                    f.write(' | -- | ------- | ----------- |\n')
+                for key in dictSource:
+                    list_tmp = dictSource[key]
+                    tmp += 1
+                    f.write(' | ' + str(tmp))
+                    target = list_tmp[0].split('.')
+                    target = target[0]
+                    problem_link = linkProblems(target)
+                    f.write(' | [{}]({}) | '.format(key, problem_link))
+                    for i in range(len(list_tmp)):
+                        if i == len(list_tmp) - 1:
+                            f.write(linkSolutions(key, list_tmp[i]) + ' ')
+                        else:
+                            f.write(linkSolutions(key, list_tmp[i]) + ', ')
                     f.write('|')
-                    print(problem_link)
-                    print(f"scrapping {tmp} from total {len(dictSource)}")
-                f.write('\n')
-            f.write('\n\n')
+                    if option.lower() == 'y':
+                        f.write(scrapper.getPoints(problem_link))
+                        f.write('|')
+                        print(problem_link)
+                        print(f"scrapping {tmp} from total {len(dictSource)}")
+                    f.write('\n')
+                f.write('\n\n')
 
-            f.write('## Author:\n')
-            f.write('- [Wyginacz Blachy](https://open.kattis.com/users/WyginaczBlachy)')
+                f.write('## Author:\n')
+                f.write('- [Wyginacz Blachy](https://open.kattis.com/users/WyginaczBlachy)')
 
-            print('\nREADME.MD Successfully Generated/Updated \n')
-            userExit = input('Press ENTER to exit..')
+                print('\nREADME.MD Successfully Generated/Updated \n')
+                userExit = input('Press ENTER to exit..')
+        except IOError as e:
+            print(f"Error writing to file: {e}")
 
-        if __name__ == '__main__':
-            userInput = input("Do you want to show 'Difficulty' or not (y/n)? ")
-            writingReadme(userInput)
+# Main block to call the function
+if __name__ == '__main__':
+    userInput = input("Do you want to show 'Difficulty' or not (y/n)? ")
+    writingReadme(userInput)
